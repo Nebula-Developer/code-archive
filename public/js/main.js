41866c637735f6fd3953da69b5930b157b21ef8c
NebulaDev <nebuladev.contact@gmail.com>
2022-12-10 18:11:29 +1300

    Finished base home page

--- public/js/main.js
+++ public/js/main.js
@@ -4,6 +4,7 @@
 // On load
 $(function() {
     $("#navbar").load("/html/navbar.html");
+    $("#footer").load("/html/footer.html");
 
     $(".nav li a[href='" + window.location.pathname + "']").parent().addClass("active");
 });
