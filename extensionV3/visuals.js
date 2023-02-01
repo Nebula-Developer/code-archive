54617371b0d2563b77727814016de4154b3d6513
NebulaDev <nebuladev.contact@gmail.com>
2023-02-01 22:35:39 +1300

    Add base channel system

--- extensionV3/visuals.js
+++ extensionV3/visuals.js
@@ -10,8 +10,61 @@ function initElements() {
     msgpanel_wrapper = document.getElementById('learnplus-msgpanel-wrapper');
     toolbar = document.getElementById('learnplus-toolbar');
 
-    handleToolbarPopups();
+    handleToolbar();
     dragToScrollHorizontal($("#learnplus-msgpanel-wrapper"));
+    handleChannelVisuals();
+}
+
+function handleToolbar() {
+    handleToolbarPopups();
+    unlockToolbar();
+
+    $(".learnplus-toolbar-icon").on('click', function() {
+        if ($(this).hasClass("lp-toolbar-ignore-default")) return;
+        var active = $(".lp-toolbar-active");
+
+        if (active.length > 0 && active[0] !== this) {
+            active.removeClass("lp-toolbar-active");
+            $(".lp-called-active").removeClass("lp-called-active");
+        }
+        
+        $(this).toggleClass("lp-toolbar-active");
+
+        var opens = $(this).attr("data-opens");
+        if (opens) $("#" + opens).toggleClass("lp-called-active");
+
+        if ($(this).hasClass("lp-toolbar-active")) {
+            lockToolbar();
+        } else {
+            suggestUnlockToolbar();
+        }
+    });
+
+    $("#lock-toolbar-btn").on('click', function() {
+        $(".lp-toolbar-active").removeClass("lp-toolbar-active");
+        $(".lp-called-active").removeClass("lp-called-active");
+
+        var popup = $(this).find(".learnplus-toolbar-icon-popup");
+        var icon = $(this).find("i");
+
+        if (icon.hasClass("fa-lock")) unlockToolbar();
+        else lockToolbar(true);
+
+        if (toolbarLocked) {
+            popup.text("Unlock Toolbar");
+            icon.removeClass("fa-unlock");
+            icon.addClass("fa-lock");
+        }
+        else {
+            popup.text("Lock Toolbar");
+            icon.removeClass("fa-lock");
+            icon.addClass("fa-unlock");
+        }
+
+        console.log(toolbarLocked);
+    });
+
+    handleLoginForm();
 }
 
 function handleToolbarPopups() {
@@ -36,62 +89,15 @@ function handleToolbarPopups() {
     });
 }
 
-function dragToScrollHorizontal(jqueryElm) {
-    var elm = jqueryElm[0];
-    var isDragging = false;
-    var lastX = 0;
-    var lastScrollLeft = 0;
-
-    jqueryElm.on('mousedown', function(e) {
-        // Make sure we are not selecting a child
-        if (e.target !== elm && !e.target.classList.contains('lp-msg-panel')) {
-            console.log(e.target);
-            console.log(elm);
-            return;
-        }
-
-        e.preventDefault();
-        isDragging = true;
-        lastX = e.clientX;
-        lastScrollLeft = elm.scrollLeft;
-
-        var wasLocked = toolbarLocked;
-        lockToolbar();
-        
-        $(document).on('selectstart', function() {
-            return false;
-        });
-
-        function revert() {
-            $(document).off('selectstart');
-            $(document).off('mousemove');
-
-            if (!wasLocked) {
-                unlockToolbar();
-            }
-            
-            isDragging = false;
-        }
-
-        $(document).on('mouseup', revert);
-        $(document).on('mouseleave', revert);
-
-        $(document).on('mousemove', function(e) {
-            if (isDragging) {
-                var deltaX = e.clientX - lastX;
-                elm.scrollLeft = lastScrollLeft - deltaX;
-            }
-        });
-    });
-}
-
 function setCSSVar(varName, value) {
     document.documentElement.style.setProperty(varName, value);
 }
 
 var toolbarLocked = false;
+var forceLockToolbar = false;
 
-function lockToolbar() {
+function lockToolbar(force) {
+    if (force) forceLockToolbar = true;
     if (toolbarLocked) return;
     toolbarLocked = true;
     $(".lp-raise-for-toolbar").addClass("lp-raise-for-toolbar-locked");
@@ -101,6 +107,151 @@ function lockToolbar() {
 function unlockToolbar() {
     if (!toolbarLocked) return;
     toolbarLocked = false;
+    forceLockToolbar = false;
     $(".lp-raise-for-toolbar").removeClass("lp-raise-for-toolbar-locked");
     $("#learnplus-toolbar").removeClass("learnplus-toolbar-locked");
+}
+
+function suggestUnlockToolbar() {
+    if (forceLockToolbar) return;
+    unlockToolbar();
+}
+
+function toggleToolbar() {
+    if (toolbarLocked) {
+        unlockToolbar();
+    } else {
+        lockToolbar(true);
+    }
+}
+
+function handleLoginForm() {
+    $("#learnplus-show-register").on('click', showRegisterForm);
+    $("#learnplus-show-login").on('click', showLoginForm);
+    showLoginForm();
+
+    $("#learnplus-login-form").on('submit', function(e) {
+        var username = $("#learnplus-login-username").val();
+        var password = $("#learnplus-login-password").val();
+
+        if (username.length === 0 || password.length === 0) {
+            showAlert("Please fill in all fields.")
+            return;
+        }
+
+        socket.emit('login', {
+            username: username,
+            password: password
+        }, (res) => {
+            if (res.success) {
+                showAlert("Logged in successfully.");
+                swapToLogoutFromForm();
+
+                var token = res.data.token;
+                chrome.storage.sync.set({token: token}, function() {
+                    logMsg("Token saved to storage.");
+                });
+            } else {
+                showAlert(res.error);
+            }
+        });
+    });
+
+    $("#learnplus-register-form").on('submit', function(e) {
+        var username = $("#learnplus-register-username").val();
+        var email = $("#learnplus-register-email").val();
+        var password = $("#learnplus-register-password").val();
+        var passwordConfirm = $("#learnplus-register-password-confirm").val();
+
+        if (username.length === 0 || email.length === 0 || password.length === 0 || passwordConfirm.length === 0) {
+            showAlert("Please fill in all fields.");
+            return;
+        }
+
+        if (password !== passwordConfirm) {
+            showAlert("Passwords do not match.");
+            return;
+        }
+        
+        if (password.length < 8) {
+            showAlert("Password must be at least 8 characters long.");
+            return;
+        }
+
+        socket.emit('register', {
+            username: username,
+            email: email,
+            password: password
+        }, (res) => {
+            if (res.success) {
+                showAlert("Account created!");
+                swapToLogoutFromForm();
+
+                var token = res.data.token;
+                chrome.storage.sync.set({token: token}, function() {
+                    logMsg("Token saved to storage.");
+                });
+            } else {
+                showAlert(res.error);
+            }
+        });
+    });
+
+    chrome.storage.sync.get(['token'], function(result) {
+        var token = result.token;
+        if (token) {
+            socket.emit('loginToken', {
+                token: token
+            }, (res) => {
+                if (!res.success) {
+                    chrome.storage.sync.remove('token', function() {
+                        logMsg("Token removed from storage due to authentication failure.");
+                        $("#learnplus-logout-btn").hide();
+                    });
+                } else {
+                    logMsg("Token authentication successful.");
+                    $("#learnplus-login-btn").hide();
+                    loadChannelBrowser();
+                }
+            });
+        } else {
+            $("#learnplus-logout-btn").hide();
+        }
+    });
+
+    $("#learnplus-logout-btn").on('click', function() {
+        chrome.storage.sync.remove('token', function() {
+            logMsg("Token removed from storage.");
+            location.reload();
+        });
+    });
+}
+
+function showLoginForm() {
+    $("#learnplus-login-form").show();
+    $("#learnplus-register-form").hide();
+}
+
+function showRegisterForm() {
+    $("#learnplus-login-form").hide();
+    $("#learnplus-register-form").show();
+}
+
+function showAlert(msg) {
+    alert(msg);
+}
+
+function swapToLogoutFromForm() {
+    $("#learnplus-login-btn").hide();
+    $("#learnplus-logout-btn").show();
+    $(".lp-toolbar-active").removeClass("lp-toolbar-active");
+    $(".lp-called-active").removeClass("lp-called-active");
+    suggestUnlockToolbar();
+}
+
+function handleChannelVisuals() {
+    $("#lp-create-channel-form").on('submit', function(e) {
+        var channelName = $("#lp-create-channel-name").val();
+        createChannel(channelName, true);
+    });
 }
--- server/index.js
+++ server/index.js
@@ -10,6 +10,18 @@ setInterval(() => {
     uptime = Math.round(process.uptime());
 }, 1000);
 
+const private = path.join(__dirname, 'private');
+const users = path.join(private, 'users.json');
+const channels = path.join(private, 'channels.json');
+
+if (!fs.existsSync(private)) fs.mkdirSync(private);
+if (!fs.existsSync(users)) fs.writeFileSync(users, JSON.stringify([]));
+if (!fs.existsSync(channels)) fs.writeFileSync(channels, JSON.stringify([]));
+function getUsers() { return JSON.parse(fs.readFileSync(users)); }
+function saveUsers(userData) { fs.writeFileSync(users, JSON.stringify(userData, null, 4)); }
+function getChannels() { return JSON.parse(fs.readFileSync(channels)); }
+function saveChannels(channelData) { fs.writeFileSync(channels, JSON.stringify(channelData, null, 4)); }
+
 const server = http.createServer((req, res) => {
     res.end(`
         <style>
@@ -56,7 +68,244 @@ io.on('connection', (socket) => {
             callback(error('File not found.'));
         }
     });
+
+    socket.on('register', (data, callback) => {
+        if (!data.username || !data.email || !data.password) {
+            callback(error('Please fill in all fields.'));
+            return;
+        }
+
+        var users = getUsers();
+        var user = users.find(u => u.username.toLowerCase() === data.username.toLowerCase() || u.email.toLowerCase() === data.email.toLowerCase());
+        if (user) {
+            callback(error('Username or email already exists.'));
+            return;
+        }
+
+        var salt = crypto.randomBytes(16).toString('hex');
+        var hash = crypto.pbkdf2Sync(data.password, salt, 1000, 64, 'sha512').toString('hex');
+
+        var newToken = crypto.randomBytes(16).toString('hex');
+
+        var newUser = {
+            username: data.username,
+            email: data.email.toLowerCase(),
+            salt: salt,
+            hash: hash,
+            tokens: [newToken],
+            id: crypto.randomBytes(16).toString('hex') + ":" + Date.now()
+        };
+
+        users.push(newUser);
+
+        saveUsers(users);
+        newUser.token = newToken;
+
+        callback(success(safeUser(newUser)));
+        socket.token = newToken;
+    });
+
+    socket.on('login', (data, callback) => {
+        if (!data.username || !data.password) {
+            callback(error('Please fill in all fields.'));
+            return;
+        }
+
+        var users = getUsers();
+        var user = users.find(u => u.username.toLowerCase() === data.username.toLowerCase());
+        if (!user) {
+            callback(error('Username or password is incorrect.'));
+            return;
+        }
+
+        var hash = crypto.pbkdf2Sync(data.password, user.salt, 1000, 64, 'sha512').toString('hex');
+        if (hash !== user.hash) {
+            callback(error('Username or password is incorrect.'));
+            return;
+        }
+
+        var newToken = crypto.randomBytes(16).toString('hex');
+        user.tokens.push(newToken);
+        if (user.tokens.length > 5) user.tokens.shift();
+
+        saveUsers(users);
+        user.token = newToken;
+
+        callback(success(safeUser(user)));
+        socket.token = newToken;
+    });
+
+    socket.on('loginToken', (data, callback) => {
+        if (!data.token) {
+            callback(error('Please fill in all fields.'));
+            return;
+        }
+
+        var users = getUsers();
+        var user = users.find(u => u.tokens.includes(data.token));
+        if (!user) {
+            callback(error('Invalid token.'));
+            return;
+        }
+
+        callback(success(safeUser(user)));
+        socket.token = data.token;
+    });
+    
+    socket.on('sendMessage', (data, callback) => {
+        if (!data.message || !data.channel) {
+            callback(error('Please fill in all fields.'));
+            return;
+        }
+
+        var user = getSocketAccount(socket);
+        if (!user) {
+            callback(error('Invalid token.'));
+            return;
+        }
+
+        var channels = getChannels();
+        var channel = channels.find(c => c.id === data.channel);
+        if (!channel) {
+            callback(error('Invalid channel.'));
+            return;
+        }
+
+        var message = {
+            id: crypto.randomBytes(16).toString('hex') + ":" + Date.now(),
+            sender: user.id,
+            message: data.message,
+            timestamp: Date.now()
+        };
+
+        channel.messages.push(message);
+        saveChannels(channels);
+        message.channel = channel.id;
+        message.username = user.username;
+        io.to(data.channel).emit('message', message);
+        callback(success(message));
+    });
+
+    socket.on('getMessages', (data, callback) => {
+        if (!data.channel) {
+            callback(error('Please fill in all fields.'));
+            return;
+        }
+
+        var user = getSocketAccount(socket);
+        if (!user) {
+            callback(error('Invalid token.'));
+            return;
+        }
+
+        var channel = getChannel(data.channel);
+        if (!channel) {
+            callback(error('Invalid channel.'));
+            return;
+        }
+
+        var messages = channel.messages.map(m => {
+            var user = getUsers().find(u => u.id === m.sender);
+            m.username = user.username;
+            return m;
+        });
+
+        callback(success(messages));
+    });
+
+    socket.on('joinChannel', (data, callback) => {
+        if (!data.channel) {
+            callback(error('Please fill in all fields.'));
+            return;
+        }
+
+        var user = getSocketAccount(socket);
+        if (!user) { callback(error('Invalid token.')); return; }
+        var channel = getChannel(data.channel);
+        if (!channel) { callback(error('Invalid channel.')); return; }
+
+        socket.join(data.channel);
+        var messages = channel.messages.map(m => {
+            var user = getUsers().find(u => u.id === m.sender);
+            m.username = user.username;
+            return m;
+        });
+
+        callback(success(messages));
+    });
+
+    socket.on('leaveChannel', (data, callback) => {
+        if (!data.channel) {
+            callback(error('Please fill in all fields.'));
+            return;
+        }
+
+        var user = getSocketAccount(socket);
+        if (!user) { callback(error('Invalid token.')); return; }
+        var channel = getChannel(data.channel);
+        if (!channel) { callback(error('Invalid channel.')); return; }
+
+        socket.leave(data.channel);
+        callback(success());
+    });
+
+    socket.on('createChannel', (data, callback) => {
+        if (!data.name) {
+            callback(error('Please fill in all fields.'));
+            return;
+        }
+
+        var user = getSocketAccount(socket);
+        if (!user) { callback(error('Invalid token.')); return; }
+
+        var channels = getChannels();
+        var channel = channels.find(c => c.name.toLowerCase() === data.name.toLowerCase());
+        if (channel) { callback(error('Channel already exists.')); return; }
+
+        var newChannel = {
+            id: crypto.randomBytes(16).toString('hex') + ":" + Date.now(),
+            name: data.name,
+            messages: []
+        };
+
+        channels.push(newChannel);
+        saveChannels(channels);
+
+        callback(success(newChannel));
+    });
+
+    socket.on('getChannels', (data, callback) => {
+        var user = getSocketAccount(socket);
+        if (!user) { callback(error('Invalid token.')); return; }
+
+        var channels = getChannels();
+        // Delete the messages from the channels
+        channels = channels.map(c => {
+            delete c.messages;
+            return c;
+        });
+        callback(success(channels));
+    });
 });
+
+function safeUser(user) {
+    delete user.salt;
+    delete user.hash;
+    delete user.tokens;
+    return user;
+}
+
+function getSocketAccount(socket) {
+    var users = getUsers();
+    var user = users.find(u => u.tokens.includes(socket.token));
+    return user;
+}
+
+function getChannel(id) {
+    var channels = getChannels();
+    var channel = channels.find(c => c.id === id);
+    return channel;
+}
 
 server.listen(port, () => {
     console.log(`listening on *:${port}`);
