b9a1b71ba56aefb24473d03067a6b0eaa5d11bd7
NebulaDev <nebuladev.contact@gmail.com>
2023-04-02 17:27:04 +1200

    Push from removal of .gitignore files

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+const path = require('path');
+const returns = require('./src/returns');
+const fs = require('fs');
+
+const port = 3002;
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log("Got connection");
+
+    socket.on('get', (p, callback) => {
+        if (!callback || typeof callback !== 'function') return;
+        if (!p || typeof p !== 'string') return callback(returns.error("Invalid path."));
+        const relativePath = path.resolve(path.join(__dirname, 'public', p));
+        if (!relativePath.startsWith(path.resolve(__dirname, 'public'))) return callback(returns.error("Invalid path."));
+        if (!fs.existsSync(relativePath)) return callback(returns.error("File does not exist."));
+        if (fs.statSync(relativePath).isDirectory()) return callback(returns.error("Cannot get directory."));
+
+        const data = fs.readFileSync(relativePath, 'utf8');
+        callback(returns.success(data));
+    });
+});
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+server.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
