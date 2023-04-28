adccdcf28a8a58944e61ad307cc6a520b0e32c29
NebulaDev <nebuladev@NebulaDevs-MacBook-Air.local>
2023-04-28 20:25:46 +1200

    Implement command substitution

--- src/exec.cpp
+++ src/exec.cpp
@@ -29,6 +29,25 @@ std::vector<std::string> handle_input(std::string input) {
     std::string quote_regex = "\"[^\"]*\"";
     std::string quote_placeholder_regex = std::string(quote_placeholder_prefix) + "[0-9]+";
 
+    std::string command_substitution_regex = "\\$\\(.*\\)";
+
+    // Handle command substitution
+    std::regex command_substitution_regex_obj(command_substitution_regex);
+    std::smatch command_substitution_match;
+
+    while (std::regex_search(input, command_substitution_match, command_substitution_regex_obj)) {
+        std::string command_substitution = command_substitution_match.str(0);
+        std::string command = command_substitution.substr(2, command_substitution.length() - 3);
+
+        std::tuple<int, std::string> command_res = execute_foutput(command);
+        std::string command_output = std::get<1>(command_res);
+
+        input.replace(command_substitution_match.position(0), command_substitution.length(), command_output);
+
+        // Trim
+        input = input.substr(0, input.find_last_not_of(" \n\r\t") + 1);
+    }
+
     // Replace quotes with placeholders
     std::vector<std::tuple<std::string, std::string>> quotes;
     std::regex quote_regex_obj(quote_regex);
@@ -75,7 +94,6 @@ std::vector<std::string> handle_input(std::string input) {
     return tokens;
 }
 
-
 int execute_primary(std::string input) {
     std::vector<std::string> args = handle_input(input);
 
@@ -101,14 +119,65 @@ int execute_primary(std::string input) {
         }
 
         exit(0);
-    }
-    else if (pid == -1) {
+    } else if (pid == -1) {
         std::cout << "shell: fork: error" << std::endl;
         return 1;
-    }
-    else {
+    } else {
         int status;
         waitpid(pid, &status, 0);
         return status;
+    }
+}
+
+std::tuple<int, std::string> execute_foutput(std::string input) {
+    std::vector<std::string> args = handle_input(input);
+
+    if (handle_integrated_check(args) == true) {
+        return std::make_tuple(0, "");
+    }
+
+    char** argv = stringv_to_charv(args);
+
+    int pipefd[2];
+    if (pipe(pipefd) == -1) {
+        std::cout << "shell: pipe: error" << std::endl;
+        return std::make_tuple(1, "");
+    }
+
+    pid_t pid = fork();
+    if (pid == 0) {
+        rl_bind_key('\t', rl_insert);
+        rl_initialize();
+
+        setenv("GIT_TERMINAL_PROMPT", "0", 1);
+
+        close(pipefd[0]);
+        dup2(pipefd[1], STDOUT_FILENO);
+
+        int execres = execvp(argv[0], argv);
+        if (execres == -1) {
+            std::cout << "shell: " << argv[0] << ": command not found" << std::endl;
+            exit(1);
+        }
+
+        exit(0);
+    }
+    else if (pid == -1) {
+        std::cout << "shell: fork: error" << std::endl;
+        return std::make_tuple(1, "");
+    }
+    else {
+        close(pipefd[1]);
+        std::string output;
+        char buf[1024];
+        ssize_t nread;
+        while ((nread = read(pipefd[0], buf, sizeof(buf))) > 0) {
+            output.append(buf, nread);
+        }
+
+        int status;
+        waitpid(pid, &status, 0);
+
+        return std::make_tuple(status, output);
     }
 }
