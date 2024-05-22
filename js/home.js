857ade9cab3e17a831e9254e239c36e8f431b54d
NebulaDev <nebuladev.contact@gmail.com>
2024-05-22 12:38:11 +1200

    General cleanup

--- js/home.js
+++ js/home.js
@@ -1,12 +1,8 @@
+var homeAboutBackground = $('.home-about-background');
 
-
-var homeAboutBackground = $(".home-about-background");
-
-$(window).on("scroll", function() {
-    var st = $(this).scrollTop();
-    homeAboutBackground.css({
-        "background-position": st / 4 + "px " + Math.sin(st / 100) * 10 + "px" // the sin gives it a bit more of a wave effect, although it's not very noticeable
-    });
+$(window).on('scroll', function () {
+	var st = $(this).scrollTop();
+	homeAboutBackground.css({
+		'background-position': st / 4 + 'px ' + Math.sin(st / 100) * 10 + 'px', // the sin gives it a bit more of a wave effect, although it's not very noticeable
+	});
 });
-
-
--- js/main.js
+++ js/main.js
@@ -1,27 +1,32 @@
-var hoverTextContainer = $(".hover-text-container");
-var hoverTextName = $(".hover-text-name");
-var hoverTextDefinition = $(".hover-text-definition");
+var hoverTextContainer = $('.hover-text-container');
+var hoverTextName = $('.hover-text-name');
+var hoverTextDefinition = $('.hover-text-definition');
 
-$(".hover-word").on("mouseover", function() { // shows a tooltip when hovering over a word
-    var hoverWord = $(this).attr("data-define").trim();
-    var hoverWordName = $(this).text().trim();
-    hoverWordName = hoverWordName.replace(/[^a-zA-Z ]/g, ""); // removes any special characters
-    hoverWordName = hoverWordName[0].toUpperCase() + hoverWordName.slice(1); // capitalizes the first letter of the word
+$('.hover-word').on('mouseover', function () {
+	// shows a tooltip when hovering over a word
+	var hoverWord = $(this).attr('data-define').trim();
+	var hoverWordName = $(this).text().trim();
+	hoverWordName = hoverWordName.replace(/[^a-zA-Z ]/g, ''); // removes any special characters
+	hoverWordName = hoverWordName[0].toUpperCase() + hoverWordName.slice(1); // capitalizes the first letter of the word
 
-    hoverTextContainer.removeClass("hide");
-    hoverTextName.text(hoverWordName);
-    hoverTextDefinition.html(hoverWord);
+	hoverTextContainer.removeClass('hide');
+	hoverTextName.text(hoverWordName);
+	hoverTextDefinition.html(hoverWord);
 
-    var pos = $(this).offset(); // document position of the hover-word
-    var top = pos.top + $(this).height();
-    var left = pos.left + $(this).width() / 2 - hoverTextContainer.width() / 2;
-    left = Math.max(0, left); // prevents from going off the left side of the screen
+	var pos = $(this).offset(); // document position of the hover-word
+	var top = pos.top + $(this).height();
+	var left = pos.left + $(this).width() / 2 - hoverTextContainer.width() / 2;
+	left = Math.max(0, left); // prevents from going off the left side of the screen
 
-    hoverTextContainer.css({
-        top: top + "px",
-        left: left + "px"
-    });
+	hoverTextContainer.css({
+		top: top + 'px',
+		left: left + 'px',
+	});
 });
 
-$(".hover-text-container").on("mouseover", () => hoverTextContainer.removeClass("hide"));
-$(".hover-text-container, .hover-word").on("mouseout", () => hoverTextContainer.addClass("hide"));
+$('.hover-text-container').on('mouseover', () =>
+	hoverTextContainer.removeClass('hide')
+);
+$('.hover-text-container, .hover-word').on('mouseout', () =>
+	hoverTextContainer.addClass('hide')
+);
