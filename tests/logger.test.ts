4cff16b8ee1c96c532136b998e37de6265330eb8
NebulaDev <nebuladev.contact@gmail.com>
2024-09-18 10:23:57 +1200

    Refactor logger tests

--- tests/logger.test.ts
+++ tests/logger.test.ts
@@ -1,70 +1,74 @@
 import logger, { attributeObject } from "../src/logger";
 
-test("Logger Test", () => {
-  logger.debug("debug");
-  logger.log("info");
-  logger.warn("warn");
-  logger.error("error");
-});
-
-test("Attribute Test", () => {
-  expect(attributeObject({ a: 1, b: 2, c: 3 }, ["a", "c"])).toEqual({
-    a: 1,
-    c: 3,
+describe("Logger Test", () => {
+  test("Logger Test", () => {
+    logger.debug("debug");
+    logger.log("info");
+    logger.warn("warn");
+    logger.error("error");
   });
-  expect(
-    attributeObject(
-      [
-        { a: 1, b: 2, c: 3 },
-        { a: 4, b: 5, c: 6 },
-      ],
-      ["a", "c"],
-    ),
-  ).toEqual([
-    { a: 1, c: 3 },
-    { a: 4, c: 6 },
-  ]);
-});
 
-test("Attribute Test with Exclude", () => {
-  expect(attributeObject({ a: 1, b: 2, c: 3 }, ["a", "c"], ["c"])).toEqual({
-    a: 1,
+  test("Attribute Test", () => {
+    expect(attributeObject({ a: 1, b: 2, c: 3 }, ["a", "c"])).toEqual({
+      a: 1,
+      c: 3,
+    });
+
+    expect(
+      attributeObject(
+        [
+          { a: 1, b: 2, c: 3 },
+          { a: 4, b: 5, c: 6 },
+        ],
+        ["a", "c"]
+      )
+    ).toEqual([
+      { a: 1, c: 3 },
+      { a: 4, c: 6 },
+    ]);
   });
-  expect(
-    attributeObject(
-      [
-        { a: 1, b: 2, c: 3 },
-        { a: 4, b: 5, c: 6 },
-      ],
-      ["a", "c"],
-      ["c"],
-    ),
-  ).toEqual([{ a: 1 }, { a: 4 }]);
-});
 
-test("Attribute Test with nested values", () => {
-  const base = {
-    a: 1,
-    b: 2,
-    c: {
-      d: 3,
-      e: 4,
-      f: [
-        { a: 1, b: 2 },
-        { a: 3, b: 4 },
-      ],
-    },
-  };
+  test("Attribute Test with Exclude", () => {
+    expect(attributeObject({ a: 1, b: 2, c: 3 }, ["a", "c"], ["c"])).toEqual({
+      a: 1,
+    });
 
-  console.log(attributeObject(base, ["a", "c.d", "c.f.a"]));
+    expect(
+      attributeObject(
+        [
+          { a: 1, b: 2, c: 3 },
+          { a: 4, b: 5, c: 6 },
+        ],
+        ["a", "c"],
+        ["c"]
+      )
+    ).toEqual([{ a: 1 }, { a: 4 }]);
+  });
 
-  expect(
-    attributeObject(base, ["a", "c.d", "c.f.a", "does.not.exist"]),
-  ).toEqual({
-    a: 1,
-    c: {
-      d: 3,
-      f: [{ a: 1 }, { a: 3 }],
-    },
+  test("Attribute Test with nested values", () => {
+    const base = {
+      a: 1,
+      b: 2,
+      c: {
+        d: 3,
+        e: 4,
+        f: [
+          { a: 1, b: 2 },
+          { a: 3, b: 4 },
+        ],
+      },
+    };
+
+    logger.debug(attributeObject(base, ["a", "c.d", "c.f.a"]));
+
+    expect(
+      attributeObject(base, ["a", "c.d", "c.f.a", "does.not.exist"])
+    ).toEqual({
+      a: 1,
+      c: {
+        d: 3,
+        f: [{ a: 1 }, { a: 3 }],
+      },
+    });
   });
 });
